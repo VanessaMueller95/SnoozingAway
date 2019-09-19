1b786d683076190d209aead41acbe47f2b71678e
Vanessa Müller <vanessa.mueller@unitybox.de>
2019-09-19 12:45:57 +0200

    Auswertung Wasser, Blink Animation

--- Snoozing Away Unity/Snoozing Away Unity/Assets/Scripts/WalkNew.cs
+++ Snoozing Away Unity/Snoozing Away Unity/Assets/Scripts/WalkNew.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WalkNew : MonoBehaviour
 {
-    private Animator animator;
 
     private Cuboid cuboid;
 
@@ -25,11 +24,20 @@ public class WalkNew : MonoBehaviour
 
     public LayerMask mask;
 
+    bool blinkEnd = false;
+    public Animator animator;
+    public AudioManager audiomanager;
+
+    public GameObject restartMenuUI;
+    public GameObject wonMenuUI;
+    public Quaternion spreadAngle;
+
 
     // Start is called before the first frame update
     void Start()
     {
         cuboid = FindObjectOfType<Cuboid>();
+        animator = transform.Find("Snoozer-Walking").GetComponent<Animator>();
 
         StartCoroutine(StartLevel());
     }
@@ -45,21 +53,18 @@ public class WalkNew : MonoBehaviour
         Vector3 worldPos = (Vector3)Cuboid.GetPosition(currentPos, cuboid.Dimensions) * cuboid.cellSize - cuboid.CenterPoint;
         gameObject.transform.localPosition = worldPos + sideVector * cuboid.cellSize;
 
-    }
+        StartCoroutine(Blink(3, "start"));
 
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(2);
-        inAnimation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(transform.up);
-
-
-        transform.Translate(Vector3.forward * Time.deltaTime*walkingSpeed);
+        if (blinkEnd == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * walkingSpeed);
+        }
 
         //Raycast Debug
         Debug.DrawRay(transform.position + transform.up, (Quaternion.AngleAxis(-85, transform.right) * (transform.transform.forward * -1))*5, Color.yellow);
@@ -77,6 +82,13 @@ public class WalkNew : MonoBehaviour
             if (hitDown.distance < 2)
             {
                 transform.position = Vector3.Lerp(transform.position, distance, Time.deltaTime * (walkingSpeed * 4));
+            }
+            if (hitFloor.collider.gameObject.GetComponent<Cube>().code == 1)
+            {
+                Debug.Log("Water");
+                blinkEnd = false;
+                animator.enabled = false;
+                StartCoroutine(Blink(2, "water"));
             }
         }
         
@@ -155,5 +167,52 @@ public class WalkNew : MonoBehaviour
     public bool V3Equal(Vector3 a, Vector3 b)
     {
         return Vector3.SqrMagnitude(a - b) < 0.0001;
+    }
+
+    IEnumerator Blink(float waitTime, string state)
+    {
+        //Berechnung der Endzeit
+        var endTime = Time.time + waitTime;
+
+        //Holt sich alle Render-Objekte von dem Charakter
+        Renderer[] rs = GetComponentsInChildren<Renderer>();
+
+        //Schaltet die Renderer regelmäßig an und aus -> Blinken
+        while (Time.time < endTime)
+        {
+            Debug.Log("Aktiv");
+            foreach (Renderer r in rs) { r.enabled = false; }
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("Aktiv");
+            foreach (Renderer r in rs) { r.enabled = true; }
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        blinkEnd = true;
+        animator.enabled = true;
+
+        //Anhängig von dem Grund für das Blinken werden Folgeaktionen ausgeführt am Ende:
+        //Bei Start wird das Ticken aktiviert und der Timer gestartet
+        /*if (state == "start")
+        {
+            GameObject.Find("TimerCanvas").GetComponent<Timer>().timerActive = true;
+            audiomanager.Play("Ticking");
+        }*/
+
+        //Bei Wasser wird das Restart Menü aufgerufen, die Zeit angehalten und das Ticken beendet
+        if (state == "water")
+        {
+            restartMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            //audiomanager.Stop("Ticking");
+        }
+        /*
+        //Im Ziel wird das Gewonnen Menü aufgerufen, die Zeit angehalten und das Ticken beendet
+        if (state == "ziel")
+        {
+            wonMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            audiomanager.Stop("Ticking");
+        }*/
     }
 }
